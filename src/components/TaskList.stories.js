5619fdb6cb7443bb3338d7dcf345ec1f7e0b0da9
mrichards360 <markell.richards@groups360.com>
2022-10-11 10:26:15 -0500

    Testing actions

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -15,7 +15,7 @@ export const MockedState = {
     { ...TaskStories.Default.args.task, id: "3", title: "Task 3" },
     { ...TaskStories.Default.args.task, id: "4", title: "Task 4" },
     { ...TaskStories.Default.args.task, id: "5", title: "Task 5" },
-    { ...TaskStories.Default.args.task, id: "6", title: "Task 6" },
+    { ...TaskStories.Default.args.task, id: "7", title: "Task 6" },
   ],
   status: "idle",
   error: null,
