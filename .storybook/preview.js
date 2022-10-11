eeee38fe7815f97f55a08386e86eb8f63dabfde9
mrichards360 <markell.richards@groups360.com>
2022-10-11 09:54:27 -0500

    Screen and automated testing

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,12 @@
-import '../src/index.css'
+import "../src/index.css";
+
+// Registers the msw addon
+import { initialize, mswDecorator } from "msw-storybook-addon";
+
+initialize();
+
+// Provide the MSW addon decorator globally
+export const decorators = [mswDecorator];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
